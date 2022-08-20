6e1ec7d79f57bca4c84b36bc403fceea9039fdaf
Wen <1234@wen.ww>
2022-08-20 20:54:21 +0800

    master_and_hox-fix_4.0.0

--- src/main/java/com/wenlifen/git/GitTest.java
+++ src/main/java/com/wenlifen/git/GitTest.java
@@ -4,6 +4,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("fen studer git");
         System.out.println("chao studer git_idea");
+        System.out.println("master git-idea");
         System.out.println("hox-fix git-idea");
     }
 }
